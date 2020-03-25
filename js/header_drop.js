447c2e093a240a4cfacaa9a4a8456181da880170
Roman <rebrovdev@gmail.com>
2020-03-25 18:47:25 +0300

    25.03 18:47

---
+++ js/header_drop.js
@@ -0,0 +1,13 @@
+window.onscroll = function drop () {
+    const panel = document.querySelector('.customer-panel-wrap');
+    const baner = document.querySelector('.baner');
+    if ( window.pageYOffset > 100 ) {
+        panel.classList.add('header_fixed');
+        baner.style.paddingTop = 'calc(1em + 1vw)';
+    }else {
+        panel.classList.remove('header_fixed');
+        baner.style.paddingTop = '0';
+
+    }
+
+}
