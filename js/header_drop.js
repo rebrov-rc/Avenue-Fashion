85ea2f4db65b14c5c1e7d3a57abd4f0917008220
Roman <rebrovdev@gmail.com>
2020-03-25 21:12:40 +0300

    21.12

--- js/header_drop.js
+++ js/header_drop.js
@@ -1,13 +1,17 @@
 window.onscroll = function drop () {
     const panel = document.querySelector('.customer-panel-wrap');
     const baner = document.querySelector('.baner');
-    if ( window.pageYOffset > 100 ) {
+    if ( window.pageYOffset > 200 ) {
+        panel.classList.remove('opacityPanel');
         panel.classList.add('header_fixed');
         baner.style.paddingTop = 'calc(1em + 1vw)';
     }else if (window.pageYOffset === 0){
         panel.classList.remove('header_fixed');
+        // panel.classList.remove('opacityPanel');
         baner.style.paddingTop = '0';
 
+    }else if ( window.pageYOffset < 200 && window.pageYOffset > 50) {
+        // panel.classList.add('opacityPanel');
     }
 
 }
